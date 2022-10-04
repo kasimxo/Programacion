70e480e692795bf68dce312cc1755364df2dc7a8
kasimxo <113863096+kasimxo@users.noreply.github.com>
2022-10-04 19:32:57 +0200

    Add files via upload

---
+++ UD_01/Ejerciciodiez.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+//Hacer un programa que lea un número N filtrando a que sea mayor que cero y calcule su factorial
+
+public class Ejerciciodiez {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//el valor de factorial lo declaramos como long porque puede ser muy largo
+		long valorfactorial= 1;
+		
+		System.out.println("Introduce un valor mayor que cero para calcular su factorial: ");
+		
+		int N = sc.nextInt();
+		
+		while (N<1) {
+			System.out.println("El valor introducido no es válido.");
+			System.out.println("Introduce un valor mayor que cero para calcular su factorial: ");
+			N = sc.nextInt();
+		}
+		
+		//NOTA: importante empezar en 1 para no multiplicar por 0
+		for (int i = 1; i<=N; i++) {
+			valorfactorial = valorfactorial*i; 
+		}
+		
+		System.out.println("El valor de " + N + "factorial es: " + valorfactorial);
+		
+
+	}
+
+}
---
+++ UD_01/Ejerciciodos.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+//Programa que sume los pares y los impares (por separado) de los números comprendidos entre N y M 
+//se habrán leído por teclado, filtrando de manera que N<=M.
+
+public class Ejerciciodos {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int SumaPar = 0;
+		int SumaImpar = 0;
+		int N = 0;
+		int M = 0;
+		//establecemos todas las variables
+	
+		//pedimos al usuario los valores
+		System.out.println("Introduzca el valor 1:");
+		N = sc.nextInt();
+
+		
+		System.out.println("Introduzca el valor 2:");
+		M = sc.nextInt();
+		
+		//nos aseguramos de que la segunda variable sea mayor que la primera
+		while (N>M) {
+			System.out.println("Error: El valor 2 no puede ser mayor que el valor 1");
+			System.out.println("Introduzca el valor 2:");
+			M = sc.nextInt();
+		}
+		
+		//hacemos un bucle que recorra todos los números desde N hasta M
+		for (int a = N; a<=M; a++){
+			
+			//si el número es par, lo sumamos a sumaPar
+			if (a % 2 == 0) {
+				SumaPar = SumaPar + N;
+			}
+			else {
+				//si el número es impar, lo sumamos a sumaimpar
+				SumaImpar = SumaImpar + N;
+			}
+			
+		}
+		//mostramos el resultado
+		System.out.println("La división de los números pares comprendidos entre " + N + " y " + M + " es: " + SumaPar);
+		System.out.println("La división de los números impares comprendidos entre " + N + " y " + M + " es: " + SumaImpar);
+
+	}
+
+}
